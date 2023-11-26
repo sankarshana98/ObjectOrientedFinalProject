05e41f939342d88da23f96e3ea6e7e8c7cbedea6
Nitish <150980743+Nitish8594@users.noreply.github.com>
2023-11-26 10:52:02 -0500

    Revert "nithish/recommendation"
    This reverts commit a1e2dfea06c272f8d35277b981e7443ef5068080.

--- user.py
+++ user.py
@@ -1,12 +1,7 @@
-from observer import Observer
-
-class User(Observer):
+class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
     def __str__(self):
         return f"User: {self.username}"
-    
-    def update(self, message):
-        print(f"{self.name} received a notification: {message}")
--- users.py
+++
@@ -1,9 +0,0 @@
-# user.py
-from observer import Observer
-
-class User(Observer):
-    def __init__(self, name):
-        self.name = name
-
-    def update(self, message):
-        print(f"{self.name} received a notification: {message}")
