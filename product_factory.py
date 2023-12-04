df82f4767ae7fc0e905c890a77415a4ea1203f56
vishnumohan0327 <vishnumohan0327@gmail.com>
2023-12-04 03:30:08 -0500

    recommendation1

--- product_factory.py
+++ product_factory.py
@@ -1,16 +1,19 @@
 from product import Electronics, Clothing, Shoes, BeautyProduct, generate_id
 
 class ProductFactory:
+    def __init__(self):
+        self.product_counter = 1 
     def create_product(self, category, *args, **kwargs):
-        product_id = generate_id()  # Generate a unique ID for the product
+        product_id = self.product_counter
+        self.product_counter += 1
         if category == "Electronics":
-            return Electronics(product_id, *args, **kwargs)
+            return Electronics(product_id, category, *args, **kwargs)
         elif category == "Clothing":
-            return Clothing(product_id, *args, **kwargs)
+            return Clothing(product_id, category, *args, **kwargs)
         elif category == "Shoes":
-            return Shoes(product_id, *args, **kwargs)
+            return Shoes(product_id, category, *args, **kwargs)
         elif category == "Beauty":
-            return BeautyProduct(product_id, *args, **kwargs)
+            return BeautyProduct(product_id, category, *args, **kwargs)
         else:
             raise ValueError("Invalid category")
 
