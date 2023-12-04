b9aba4040d66e8f909a72729d6af3b1e1392c64c
Sankarshana Dorbala <sankarshana.dorbala@gmail.com>
2023-12-04 00:28:09 -0500

    Latest updates
    Added Order class

--- product.py
+++ product.py
@@ -1,16 +1,30 @@
+from generateId import generate_id
+
 class Product:
-    def __init__(self, name, price, description, rating):
+    def __init__(self, product_id, name, price, description, rating):
+        self.product_id = product_id
         self.name = name
         self.price = price
         self.description = description
         self.rating = rating
 
     def display_info(self):
+        print(f"Product ID: {self.product_id}")
         print(f"Name: {self.name}\nPrice: ${self.price}\nDescription: {self.description}\nRating: {self.rating}")
 
+    def serialize(self):
+        return {
+            'product_id': self.product_id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'rating': self.rating
+        }
+
+# Update subclasses to include product_id
 class Electronics(Product):
-    def __init__(self, name, price, description, rating, brand):
-        super().__init__(name, price, description, rating)
+    def __init__(self, product_id, name, price, description, rating, brand):
+        super().__init__(product_id, name, price, description, rating)
         self.brand = brand
 
     def display_info(self):
@@ -18,8 +32,8 @@ class Electronics(Product):
         print(f"Brand: {self.brand}")
 
 class Clothing(Product):
-    def __init__(self, name, price, description, rating, size):
-        super().__init__(name, price, description, rating)
+    def __init__(self, product_id, name, price, description, rating, size):
+        super().__init__(product_id, name, price, description, rating)
         self.size = size
 
     def display_info(self):
@@ -27,8 +41,8 @@ class Clothing(Product):
         print(f"Size: {self.size}")
 
 class Shoes(Product):
-    def __init__(self, name, price, description, rating, size):
-        super().__init__(name, price, description, rating)
+    def __init__(self, product_id, name, price, description, rating, size):
+        super().__init__(product_id, name, price, description, rating)
         self.size = size
 
     def display_info(self):
@@ -36,8 +50,8 @@ class Shoes(Product):
         print(f"Size: {self.size}")
 
 class BeautyProduct(Product):
-    def __init__(self, name, price, description, rating, brand):
-        super().__init__(name, price, description, rating)
+    def __init__(self, product_id, name, price, description, rating, brand):
+        super().__init__(product_id, name, price, description, rating)
         self.brand = brand
 
     def display_info(self):
--- user.py
+++ user.py
@@ -1,7 +1,22 @@
+# class User:
+#     def __init__(self, username, password):
+#         self.username = username
+#         self.password = password
+
+#     def __str__(self):
+#         return f"User: {self.username}"
+
+# user.py
+
 class User:
-    def __init__(self, username, password):
+    def __init__(self, user_id, username, password, full_name="", email="", profile_pic=None):
+        self.user_id = user_id
         self.username = username
         self.password = password
+        self.full_name = full_name
+        self.email = email
+        self.profile_pic = profile_pic
 
     def __str__(self):
-        return f"User: {self.username}"
+        return f"User: {self.username} ({self.full_name}), Email: {self.email}, UserID: {self.user_id}"
+
